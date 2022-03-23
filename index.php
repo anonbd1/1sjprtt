ccdf655ebeb70ce4a0ff8e575489039e985f68f2
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-03-23 08:03:25 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>Checker</title>
+
+
+<!--[if lt IE 10]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="Use our premium tools for free to create your free premium accounts." />
+<meta name="keywords" content="cc checker,ccgen,rso blog,ros checker,credit card,cc,credit card checker,credit card generator">
+
+<link rel="icon" href="png/logo.png" type="image/x-icon">
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<script>
+var myVar=setInterval(function(){myTimer()},1000);
+function myTimer() {
+    var d = new Date();
+    document.getElementById("horas").innerHTML = d.toLocaleTimeString();
+}
+</script>
+</head>
+<body>
+
+<div class="pcoded-content">
+<div class="row">
+
+<div class="col-md-12 col-xl-8">
+<div class="card sale-card">
+<div class="card-header">
+<h5>Checker - NON Sk<label class="badge badge-info">#Beast</label></h5>
+</div>
+<div class="card-block">
+<textarea id="lista" class="form-control" rows="14" cols="5"></textarea>
+<br />
+<input type="text" class="form-control" id="tok"placeholder="Enter your rso_tok_ here." value="" required="required">
+<br />
+<center><button class="btn waves-effect waves-light btn-grd-success btn-block" id="
+iniciar" onclick="enviar()">Start</button>
+   
+<button class="btn waves-effect waves-light btn-grd-danger btn-block" id="parar" onclick="stop()">Stop</button>
+  </center>
+ 
+</div>
+</div>
+</div>
+<div class="col-md-12 col-xl-4">
+<div class="card comp-card">
+<div class="card-body">
+<div class="row align-items-center">
+<div class="col">
+<h6 class="m-b-25">LIVE</h6>
+<h3 class="f-w-700 text-c-green" id="CLIVE">0</h3>
+
+</div>
+<div class="col-auto">
+<i class="fas fa-check bg-c-green"></i>
+</div>
+ </div>
+</div>
+</div>
+<div class="card comp-card">
+<div class="card-body">
+<div class="row align-items-center">
+<div class="col">
+<h6 class="m-b-25">DEAD</h6>
+<h3 class="f-w-700 text-c-red" id="CDIE">0</h3>
+
+</div>
+<div class="col-auto">
+<i class="fas fa-times bg-c-red"></i>
+</div>
+ </div>
+</div>
+</div>
+<div class="card comp-card">
+<div class="card-body">
+<div class="row align-items-center">
+<div class="col">
+<h6 class="m-b-25">CHECKED</h6>
+<h3 class="f-w-700 text-c-yellow" id="testado">0</h3>
+
+</div>
+<div class="col-auto">
+<i class="fas fa-hand-paper bg-c-yellow"></i>
+</div>
+</div>
+</div>
+</div>
+<div class="card comp-card">
+<div class="card-body">
+<div class="row align-items-center">
+<div class="col">
+<h6 class="m-b-25">TOTAL</h6>
+<h3 class="f-w-700 text-c-blue" id="carregada">0</h3>
+
+</div>
+<div class="col-auto">
+<i class="fas fa-credit-card bg-c-blue"></i>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div><center>
+      
+</center>
+</div>
+<br>
+
+<div class="card sale-card">
+<div class="card-header">
+<h5><label class="label label-success">Live</label> <label class="badge badge-success" id="cLive2">0</label></h5>
+</div>
+<div class="card-block">
+	<div id="bode">
+        </div>
+        <span id="aprovadasapp" class="aprovadas"></span>
+
+	
+</div>
+</div>
+
+<div class="card sale-card">
+<div class="card-header">
+<h5><label class="label label-danger">DEAD</label> <label class="badge badge-danger" id="cDie2">0</label></h5>
+</div>
+<div class="card-block">
+<div id="bode2">
+        </div>
+        <span id="reprovadasapp" class="reprovadas"></span>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<center>&copy; 2022 - <a href="https://anonbd.pro" target="_blank">@ANONBD</a>
+<br>
+Powered By : <a href="https://t.me/hackerbd1" target="_blank">@HACKERBD1</a>
+</center>
+<br>
+
+</div>
+</div>
+</div>
+</div>
+
+
+<!--[if lt IE 10]>
+    <div class="ie-warning">
+        <h1>Warning!!</h1>
+        <p>You are using an outdated version of Internet Explorer, please upgrade
+            <br/>to any of the following web browsers to access this website.
+        </p>
+        <div class="iew-container">
+            <ul class="iew-download">
+                <li>
+                    <a href="http://www.google.com/chrome/">
+                        <img src="../files/assets/images/browser/chrome.png" alt="Chrome">
+                        <div>Chrome</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.mozilla.org/en-US/firefox/new/">
+                        <img src="../files/assets/images/browser/firefox.png" alt="Firefox">
+                        <div>Firefox</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.opera.com">
+                        <img src="../files/assets/images/browser/opera.png" alt="Opera">
+                        <div>Opera</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.apple.com/safari/">
+                        <img src="../files/assets/images/browser/safari.png" alt="Safari">
+                        <div>Safari</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                        <img src="../files/assets/images/browser/ie.png" alt="">
+                        <div>IE (9 & above)</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>Sorry for the inconvenience!</p>
+    </div>
+<![endif]-->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+<script src="jquery.js"></script>
+</body> 
+</html>
